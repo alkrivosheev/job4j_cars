df8031b0f50ced86d181b7ef13e1e64981eb233c
alkrivosheev <alkrivosheev@gmail.com>
2025-10-15 10:13:53 +0300

    add filters

---
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -0,0 +1,51 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import ru.job4j.cars.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+public class PostRepository {
+    private final CrudRepository crudRepository;
+
+    /**
+     * Показать объявления за последний день.
+     * @return список объявлений.
+     */
+    public List<Post> findPostsForLastDay() {
+        LocalDateTime oneDayAgo = LocalDateTime.now().minusDays(1);
+        String query = "SELECT DISTINCT p FROM Post p WHERE p.created > :oneDayAgo ORDER BY p.created DESC";
+        return crudRepository.query(query, Post.class, Map.of("oneDayAgo", oneDayAgo));
+    }
+
+    /**
+     * Показать объявления с фото.
+     * @return список объявлений с фото.
+     */
+    public List<Post> findPostsWithPhoto() {
+        String query = """
+            SELECT DISTINCT p FROM Post p
+            JOIN p.photos ph
+            ORDER BY p.created DESC
+            """;
+        return crudRepository.query(query, Post.class);
+    }
+
+    /**
+     * Показать объявления определенной марки.
+     * @param brand марка автомобиля.
+     * @return список объявлений.
+     */
+    public List<Post> findPostsByBrand(String brand) {
+        String query = """
+            SELECT DISTINCT p FROM Post p
+            JOIN p.car c
+            WHERE c.name LIKE :brand
+            ORDER BY p.created DESC
+            """;
+        return crudRepository.query(query, Post.class, Map.of("brand", "%" + brand + "%"));
+    }
+}
