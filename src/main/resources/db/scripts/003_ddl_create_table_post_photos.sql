2b2e519512cc7eb2b66ae66b3b166157992612ee
alkrivosheev <alkrivosheev@gmail.com>
2025-10-28 14:46:35 +0300

    add DB_diagram & SQL scripts

--- src/main/resources/db/scripts/003_ddl_create_table_post_photos.sql
+++ src/main/resources/db/scripts/003_ddl_create_table_post_photos.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS post_photos (
                              id BIGSERIAL PRIMARY KEY,
-                             post_id BIGINT NOT NULL REFERENCES auto_post(id) ON DELETE CASCADE,
+                             post_id BIGINT NOT NULL REFERENCES posts(id) ON DELETE CASCADE,
                              photo_path VARCHAR(255) NOT NULL
 );
